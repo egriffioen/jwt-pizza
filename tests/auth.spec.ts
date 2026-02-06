f621c3b0b1f02f991f37e51cf8df5e3702a6ca8a
egriffioen <ellagriffioen@gmail.com>
2026-02-05 22:06:17 -0700

    First test running

---
+++ tests/auth.spec.ts
@@ -0,0 +1,18 @@
+//import { test, expect } from '@playwright/test';
+import { test, expect } from 'playwright-test-coverage';
+
+// test('test', async ({ page }) => {
+//   await page.goto('http://localhost:5173/');
+
+//   await page.getByRole('link', { name: 'Register' }).click();
+//   await page.getByRole('textbox', { name: 'Full name' }).fill('bob');
+//   await page.getByRole('textbox', { name: 'Email address' }).fill('bob@gmail.com');
+//   await page.getByRole('textbox', { name: 'Password' }).fill('monkey');
+//   await page.getByRole('button', { name: 'Register' }).click();
+// });
+
+test('home page', async ({ page }) => {
+  await page.goto('/');
+
+  expect(await page.title()).toBe('JWT Pizza');
+});
