eaa867477219f1d277e3cfe458ea3ddef04c3675
egriffioen <ellagriffioen@gmail.com>
2026-02-05 22:25:18 -0700

    add(register user) test

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -16,3 +16,18 @@ test('home page', async ({ page }) => {
 
   expect(await page.title()).toBe('JWT Pizza');
 });
+
+test('register a user', async ({ page }) => {
+  await page.goto('/');
+
+  await page.getByRole('link', { name: 'Register' }).click();
+  await page.getByRole('textbox', { name: 'Full name' }).click();
+  await page.getByRole('textbox', { name: 'Full name' }).fill('Ella');
+  await page.getByRole('textbox', { name: 'Email address' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('ellagriffioen@test.com');
+  await page.getByRole('textbox', { name: 'Email address' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).fill('123');
+  await page.getByRole('button', { name: 'Register' }).click();
+  await page.getByText('The web\'s best pizza', { exact: true }).click();
+  await page.getByText('JWT Pizza', { exact: true }).click();
+});
