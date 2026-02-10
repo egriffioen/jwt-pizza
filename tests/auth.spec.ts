fee431e6d7684e9e348bb2d70dd00833aacf69d6
egriffioen <ellagriffioen@gmail.com>
2026-02-09 22:22:42 -0700

    add(login mock)

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -1,5 +1,7 @@
 //import { test, expect } from '@playwright/test';
+import { Page } from '@playwright/test';
 import { test, expect } from 'playwright-test-coverage';
+import { User, Role } from '../src/service/pizzaService';
 
 // test('test', async ({ page }) => {
 //   await page.goto('http://localhost:5173/');
@@ -49,3 +51,36 @@ test('login', async ({ page }) => {
   await page.getByText('ellagriffioen@test.com').click();
   await page.getByText('diner', { exact: true }).click();
 });
+
+test('login using mock', async ({ page }) => {
+  await page.goto('/');
+  await basicInit(page);
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('d@jwt.com');
+  await page.getByRole('textbox', { name: 'Password' }).fill('a');
+  await page.getByRole('button', { name: 'Login' }).click();
+  
+  await expect(page.getByRole('link', { name: 'KC' })).toBeVisible();
+});
+
+async function basicInit(page: Page) {
+  let loggedInUser: User | undefined;
+  const validUsers: Record<string, User> = { 'd@jwt.com': { id: '3', name: 'Kai Chen', email: 'd@jwt.com', password: 'a', roles: [{ role: Role.Diner }] } };
+
+  // Authorize login for the given user
+  await page.route('*/**/api/auth', async (route) => {
+    const loginReq = route.request().postDataJSON();
+    const user = validUsers[loginReq.email];
+    if (!user || user.password !== loginReq.password) {
+      await route.fulfill({ status: 401, json: { error: 'Unauthorized' } });
+      return;
+    }
+    loggedInUser = validUsers[loginReq.email];
+    const loginRes = {
+      user: loggedInUser,
+      token: 'abcdef',
+    };
+    expect(route.request().method()).toBe('PUT');
+    await route.fulfill({ json: loginRes });
+  });
+}
