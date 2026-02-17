4e5153922623176924c50f549474b785f6b7295b
egriffioen <ellagriffioen@gmail.com>
2026-02-17 15:58:54 -0700

    add(test email updates persist)

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -1,81 +1,10 @@
+import { Page } from '@playwright/test';
 import { test, expect } from 'playwright-test-coverage';
+import { User, Role } from '../src/service/pizzaService';
 
-// async function basicInit(page: Page) {
-//   let loggedInUser: User | undefined;
-//   const validUsers: Record<string, User> = { 'd@jwt.com': { id: '3', name: 'Kai Chen', email: 'd@jwt.com', password: 'a', roles: [{ role: Role.Diner }] } };
-
-//   // Authorize login for the given user
-//   await page.route('*/**/api/auth', async (route) => {
-//     const req = route.request();
-//     const method = req.method();
-//     if (method === 'POST') {
-//       const { name, email, password } = req.postDataJSON();
-
-//       if (validUsers[email]) {
-//         await route.fulfill({
-//           status: 409,
-//           json: { error: 'User already exists' },
-//         });
-//         return;
-//       }
-//       const newUser: User = {
-//         id: String(Object.keys(validUsers).length + 1),
-//         name,
-//         email,
-//         password,
-//         roles: [{ role: Role.Diner }],
-//       };
-//       validUsers[email] = newUser;
-//       loggedInUser = newUser;
-
-//       await route.fulfill({
-//         json: {
-//           user: {
-//             id: newUser.id,
-//             name: newUser.name,
-//             email: newUser.email,
-//             roles: newUser.roles,
-//           },
-//           token: 'tttttt',
-//         },
-//       });
-//     }
-//     if (method === 'PUT') {
-//       const loginReq = route.request().postDataJSON();
-//       const user = validUsers[loginReq.email];
-//       if (!user || user.password !== loginReq.password) {
-//         await route.fulfill({ status: 401, json: { error: 'Unauthorized' } });
-//         return;
-//       }
-//       loggedInUser = validUsers[loginReq.email];
-//       const loginRes = {
-//         user: loggedInUser,
-//         token: 'abcdef',
-//       };
-//       expect(route.request().method()).toBe('PUT');
-//       await route.fulfill({ json: loginRes });
-//     }
-//     if (method === 'DELETE') {
-//       const authHeader = req.headers()['authorization'];
-//       expect(authHeader).toMatch(/^Bearer\s.+/);
-
-//       loggedInUser = undefined;
-
-//       await route.fulfill({
-//         json: { message: 'logout successful' },
-//       });
-//     }
-//   });
-
-//   // Return the currently logged in user
-//   await page.route('*/**/api/user/me', async (route) => {
-//     expect(route.request().method()).toBe('GET');
-//     await route.fulfill({ json: loggedInUser });
-//   });
-
-test('updateUser', async ({ page }) => {
+test('updateUser username', async ({ page }) => {
+  await basicInit(page);
   const email = `user${Math.floor(Math.random() * 10000)}@jwt.com`;
-  await page.goto('/');
   await page.getByRole('link', { name: 'Register' }).click();
   await page.getByRole('textbox', { name: 'Full name' }).fill('pizza diner');
   await page.getByRole('textbox', { name: 'Email address' }).fill(email);
@@ -114,3 +43,248 @@ test('updateUser', async ({ page }) => {
 
   await expect(page.getByRole('main')).toContainText('pizza dinerx');
 });
+
+test('updateUser email', async ({ page }) => {
+  await basicInit(page);
+  let email = `user${Math.floor(Math.random() * 10000)}@jwt.com`;
+  await page.getByRole('link', { name: 'Register' }).click();
+  await page.getByRole('textbox', { name: 'Full name' }).fill('pizza diner');
+  await page.getByRole('textbox', { name: 'Email address' }).fill(email);
+  await page.getByRole('textbox', { name: 'Password' }).fill('diner');
+  await page.getByRole('button', { name: 'Register' }).click();
+
+  await page.getByRole('link', { name: 'pd' }).click();
+
+  await page.getByRole('button', { name: 'Edit' }).click();
+  await expect(page.locator('#hs-jwt-modal').getByText('email:')).toBeVisible();
+  await page.locator('input[type="email"]').click();
+  email = 'newemail@jwt.com'
+  await page.locator('input[type="email"]').fill(email);
+  await page.getByRole('button', { name: 'Update' }).click();
+
+  await page.waitForSelector('[role="dialog"].hidden', { state: 'attached' });
+
+  await expect(page.getByRole('main')).toContainText(email);
+
+  await page.getByRole('link', { name: 'Logout' }).click();
+  await page.getByRole('link', { name: 'Login' }).click();
+
+  await page.getByRole('textbox', { name: 'Email address' }).fill(email);
+  await page.getByRole('textbox', { name: 'Password' }).fill('diner');
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await page.getByRole('link', { name: 'pd' }).click();
+
+  await expect(page.getByRole('main')).toContainText(email);
+});
+
+async function basicInit(page: Page) {
+  let loggedInUser: User | undefined;
+  const validUsers: Record<string, User> = { 'd@jwt.com': { id: '3', name: 'Kai Chen', email: 'd@jwt.com', password: 'a', roles: [{ role: Role.Diner }] } };
+
+  // Authorize login for the given user
+  await page.route('*/**/api/auth', async (route) => {
+    const req = route.request();
+    const method = req.method();
+    if (method === 'POST') {
+      const { name, email, password } = req.postDataJSON();
+
+      if (validUsers[email]) {
+        await route.fulfill({
+          status: 409,
+          json: { error: 'User already exists' },
+        });
+        return;
+      }
+      const newUser: User = {
+        id: String(Object.keys(validUsers).length + 1),
+        name,
+        email,
+        password,
+        roles: [{ role: Role.Diner }],
+      };
+      validUsers[email] = newUser;
+      loggedInUser = newUser;
+
+      await route.fulfill({
+        json: {
+          user: {
+            id: newUser.id,
+            name: newUser.name,
+            email: newUser.email,
+            roles: newUser.roles,
+          },
+          token: 'tttttt',
+        },
+      });
+    }
+    if (method === 'PUT') {
+      const loginReq = route.request().postDataJSON();
+      const user = validUsers[loginReq.email];
+      if (!user || user.password !== loginReq.password) {
+        await route.fulfill({ status: 401, json: { error: 'Unauthorized' } });
+        return;
+      }
+      loggedInUser = validUsers[loginReq.email];
+      const loginRes = {
+        user: loggedInUser,
+        token: 'abcdef',
+      };
+      expect(route.request().method()).toBe('PUT');
+      await route.fulfill({ json: loginRes });
+    }
+    if (method === 'DELETE') {
+      const authHeader = req.headers()['authorization'];
+      expect(authHeader).toMatch(/^Bearer\s.+/);
+
+      loggedInUser = undefined;
+
+      await route.fulfill({
+        json: { message: 'logout successful' },
+      });
+    }
+  });
+
+  // Update user
+  await page.route(/\/api\/user\/\d+$/, async (route) => {
+    const req = route.request();
+
+    if (req.method() !== 'PUT') {
+      await route.continue();
+      return;
+    }
+
+    const authHeader = req.headers()['authorization'];
+    expect(authHeader).toMatch(/^Bearer\s.+/);
+
+    if (!loggedInUser) {
+      await route.fulfill({ status: 401, json: { error: 'Unauthorized' } });
+      return;
+    }
+
+    const updateReq = req.postDataJSON();
+    const userId = req.url().split('/').pop();
+
+    if (loggedInUser.id !== userId) {
+      await route.fulfill({ status: 403, json: { error: 'Forbidden' } });
+      return;
+    }
+
+    // Update user in memory
+    const updatedUser = {
+      ...loggedInUser,
+      ...updateReq,
+    };
+
+    loggedInUser = updatedUser;
+    validUsers[updatedUser.email] = updatedUser;
+
+    await route.fulfill({
+      json: {
+        user: {
+          id: updatedUser.id,
+          name: updatedUser.name,
+          email: updatedUser.email,
+          roles: updatedUser.roles,
+        },
+        token: 'tttttt',
+      },
+    });
+  });
+
+  // Return the currently logged in user
+  await page.route('*/**/api/user/me', async (route) => {
+    expect(route.request().method()).toBe('GET');
+    await route.fulfill({ json: loggedInUser });
+  });
+
+  // A standard menu
+  await page.route('*/**/api/order/menu', async (route) => {
+    const menuRes = [
+      {
+        id: 1,
+        title: 'Veggie',
+        image: 'pizza1.png',
+        price: 0.0038,
+        description: 'A garden of delight',
+      },
+      {
+        id: 2,
+        title: 'Pepperoni',
+        image: 'pizza2.png',
+        price: 0.0042,
+        description: 'Spicy treat',
+      },
+    ];
+    expect(route.request().method()).toBe('GET');
+    await route.fulfill({ json: menuRes });
+  });
+
+  // Standard franchises and stores
+  await page.route(/\/api\/franchise(\?.*)?$/, async (route) => {
+    const franchiseRes = {
+      franchises: [
+        {
+          id: 2,
+          name: 'LotaPizza',
+          stores: [
+            { id: 4, name: 'Lehi' },
+            { id: 5, name: 'Springville' },
+            { id: 6, name: 'American Fork' },
+          ],
+        },
+        { id: 3, name: 'PizzaCorp', stores: [{ id: 7, name: 'Spanish Fork' }] },
+        { id: 4, name: 'topSpot', stores: [] },
+      ],
+    };
+    expect(route.request().method()).toBe('GET');
+    await route.fulfill({ json: franchiseRes });
+  });
+
+  // Order a pizza.
+  await page.route('*/**/api/order', async (route) => {
+      const req = route.request();
+      const method = req.method();
+
+      if (method === 'GET') {
+        const authHeader = req.headers()['authorization'];
+        expect(authHeader).toMatch(/^Bearer\s.+/);
+
+        await route.fulfill({
+          json: {
+            dinerId: 4,
+            orders: [
+              {
+                id: 1,
+                franchiseId: 1,
+                storeId: 1,
+                date: '2024-06-05T05:14:40.000Z',
+                items: [
+                  {
+                    id: 1,
+                    menuId: 1,
+                    description: 'Veggie',
+                    price: 0.05,
+                  },
+                ],
+              },
+            ],
+            page: 1,
+          },
+        });
+      }
+
+      if (method === 'POST') {
+        const orderReq = route.request().postDataJSON();
+        const orderRes = {
+          order: { ...orderReq, id: 23 },
+          jwt: 'eyJpYXQ',
+        };
+        expect(route.request().method()).toBe('POST');
+        await route.fulfill({ json: orderRes });
+    }});
+
+
+  await page.goto('/');
+}
+
